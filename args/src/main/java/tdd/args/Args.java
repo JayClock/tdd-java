03f05bc4bec633d6dac10c0a2c578eff7572e096
JayClock <35857909+JayClock@users.noreply.github.com>
2025-03-22 21:14:22 +0800

    refactor(args): replace inline parsers with dedicated classes
    Extract string, int, and boolean parsing logic into separate classes (StringParser, IntParser, BooleanParser) implementing a common OptionParser interface. Simplify Args.java by using a map to associate types with their respective parsers

--- args/src/main/java/tdd/args/Args.java
+++ args/src/main/java/tdd/args/Args.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Parameter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class Args {
     public static <T> T parse(Class<T> optionsClass, String... args) {
@@ -17,57 +18,16 @@ public class Args {
         }
     }
 
+    private static final Map<Class<?>, OptionParser> PARSERS = Map.of(
+            boolean.class, new BooleanParser(),
+            int.class, new IntParser(),
+            String.class, new StringParser());
+
     private static Object parseOption(Parameter parameter, List<String> arguments) {
-        Object value = null;
         Option option = parameter.getAnnotation(Option.class);
-        if (parameter.getType() == boolean.class) {
-            value = parseBoolean(arguments, option);
-        }
-        if (parameter.getType() == int.class) {
-            value = parseInt(arguments, option);
-        }
-        if (parameter.getType() == String.class) {
-            value = parseString(arguments, option);
-        }
-        return value;
+        Class<?> type = parameter.getType();
+        OptionParser parser = PARSERS.get(type);
+        return parser.parse(arguments, option);
     }
 
-    interface OptionParser {
-        Object parse(List<String> arguments, Option option);
-    }
-
-    private static Object parseString(List<String> arguments, Option option) {
-        return new StringParser().parse(arguments, option);
-    }
-
-    static class StringParser implements OptionParser {
-        @Override
-        public Object parse(List<String> arguments, Option option) {
-            int index = arguments.indexOf("-" + option.value());
-            return arguments.get(index + 1);
-        }
-    }
-
-    private static Object parseInt(List<String> arguments, Option option) {
-        return new IntParser().parse(arguments, option);
-    }
-
-    static class IntParser implements OptionParser {
-        @Override
-        public Object parse(List<String> arguments, Option option) {
-            int index = arguments.indexOf("-" + option.value());
-            return Integer.parseInt(arguments.get(index + 1));
-        }
-    }
-
-    private static Object parseBoolean(List<String> arguments, Option option) {
-        return new BooleanParser().parse(arguments, option);
-    }
-
-    static class BooleanParser implements OptionParser {
-        @Override
-        public Object parse(List<String> arguments, Option option) {
-            return arguments.contains("-" + option.value());
-        }
-    }
 }
