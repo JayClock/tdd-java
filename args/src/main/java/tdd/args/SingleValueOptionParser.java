20c7d053a519608fb383eee76bab8f08ddc2aae5
JayClock <35857909+JayClock@users.noreply.github.com>
2025-03-24 13:38:05 +0800

    refactor(args): improve argument parsing logic
    Replace manual flag checks with IntStream to handle multiple values between flags. Simplify logic for detecting insufficient or excessive arguments by extracting values between flags. Add helper methods

--- args/src/main/java/tdd/args/SingleValueOptionParser.java
+++ args/src/main/java/tdd/args/SingleValueOptionParser.java
@@ -2,6 +2,7 @@ package tdd.args;
 
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 
 class SingleValueOptionParser<T> implements OptionParser<T> {
     Function<String, T> valueParser;
@@ -20,14 +21,29 @@ class SingleValueOptionParser<T> implements OptionParser<T> {
             return defaultValue;
         }
 
-        if (index + 1 == arguments.size() || arguments.get(index + 1).startsWith("-")) {
+        int followingFlag = IntStream.range(index + 1, arguments.size()).filter(it -> arguments.get(it).startsWith("-")).findFirst().orElse(arguments.size());
+        List<String> values = arguments.subList(index + 1, followingFlag);
+
+        if (values.isEmpty()) {
             throw new InsufficientArgumentsException(option.value());
         }
 
-        if (index + 2 < arguments.size() && !arguments.get(index + 2).startsWith("-")) {
+        if (values.size() > 1) {
             throw new TooManyArgumentsException(option.value());
         }
 
         return valueParser.apply(arguments.get(index + 1));
+    }
+
+    private static boolean secondArgumentIsNotAFlag(List<String> arguments, int index) {
+        return index + 2 < arguments.size() && !arguments.get(index + 2).startsWith("-");
+    }
+
+    private static boolean isFollowByOtherFlag(List<String> arguments, int index) {
+        return arguments.get(index + 1).startsWith("-");
+    }
+
+    private static boolean isReachEndOfList(List<String> arguments, int index) {
+        return index + 1 == arguments.size();
     }
 }
