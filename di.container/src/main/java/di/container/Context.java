946ce1394136ec203ca7c573d07f7634c1fe0ca7
JayClock <35857909+JayClock@users.noreply.github.com>
2025-03-24 19:37:30 +0800

    refactor: 使用 provider factory 统一 instance 和 implementation

--- di.container/src/main/java/di/container/Context.java
+++ di.container/src/main/java/di/container/Context.java
@@ -1,30 +1,28 @@
 package di.container;
 
+import jakarta.inject.Provider;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Context {
-    private final Map<Class<?>, Object> components = new HashMap<>();
-    private final Map<Class<?>, Class<?>> componentImplementations = new HashMap<>();
+    private final Map<Class<?>, Provider<?>> providers = new HashMap<>();
 
     public <ComponentType> void bind(Class<ComponentType> type, ComponentType instance) {
-        components.put(type, instance);
+        providers.put(type, (Provider<ComponentType>) () -> instance);
     }
 
     public <ComponentType, ComponentImplementation extends ComponentType> void bind(Class<ComponentType> type, Class<ComponentImplementation> implementation) {
-        componentImplementations.put(type, implementation);
+        providers.put(type, (Provider<ComponentType>) () -> {
+            try {
+                return (ComponentType) ((Class<?>) implementation).getConstructor().newInstance();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     public <ComponentType> ComponentType get(Class<ComponentType> type) {
-        if (components.containsKey(type)) {
-            return (ComponentType) components.get(type);
-        }
-        Class<?> implementation = componentImplementations.get(type);
-        try {
-            return (ComponentType) implementation.getConstructor().newInstance();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        return (ComponentType) providers.get(type).get();
     }
-
 }
