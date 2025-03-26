7d31cf93824ca326723155e37e53acb4acb45bcf
JayClock <35857909+JayClock@users.noreply.github.com>
2025-03-26 13:22:35 +0800

    refactor: 移动 dependencies 至 providers

--- di.container/src/main/java/di/container/ContextConfig.java
+++ di.container/src/main/java/di/container/ContextConfig.java
@@ -1,33 +1,31 @@
 package di.container;
 
-import jakarta.inject.Inject;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Parameter;
 import java.util.*;
-import java.util.stream.Collectors;
-
-import static java.util.Arrays.stream;
 
 public class ContextConfig {
     private final Map<Class<?>, ComponentProvider<?>> providers = new HashMap<>();
-    private final Map<Class<?>, List<Class<?>>> dependencies = new HashMap<>();
 
     public <Type> void bind(Class<Type> type, Type instance) {
-        providers.put(type, context -> instance);
-        dependencies.put(type, List.of());
+        providers.put(type, new ComponentProvider<>() {
+            @Override
+            public Object get(Context context) {
+                return instance;
+            }
+
+            @Override
+            public List<Class<?>> getDependencies() {
+                return List.of();
+            }
+        });
     }
 
     public <Type, Implementation extends Type>
     void bind(Class<Type> type, Class<Implementation> implementation) {
-        Constructor<Implementation> injectConstructor = getInjectConstructor(implementation);
-        providers.put(type, new ConstructorInjectionProvider<Implementation>(type, injectConstructor));
-        dependencies.put(type, stream(injectConstructor.getParameters()).map(Parameter::getType).collect(Collectors.toList()));
+        providers.put(type, new ConstructorInjectionProvider<>(implementation));
     }
 
     public Context getContext() {
-        dependencies.keySet().forEach(component -> checkDependencies(component, new Stack<>()));
+        providers.keySet().forEach(component -> checkDependencies(component, new Stack<>()));
         return new Context() {
             @Override
             public <Type> Optional<Type> get(Class<Type> type) {
@@ -37,8 +35,8 @@ public class ContextConfig {
     }
 
     private void checkDependencies(Class<?> component, Stack<Class<?>> visiting) {
-        for (Class<?> dependency : dependencies.get(component)) {
-            if (!dependencies.containsKey(dependency)) throw new DependencyNotFoundException(dependency, component);
+        for (Class<?> dependency : providers.get(component).getDependencies()) {
+            if (!providers.containsKey(dependency)) throw new DependencyNotFoundException(dependency, component);
             if (visiting.contains(dependency)) throw new CyclicDependenciesFoundException(visiting);
             visiting.push(dependency);
             checkDependencies(dependency, visiting);
@@ -48,41 +46,7 @@ public class ContextConfig {
 
     interface ComponentProvider<T> {
         T get(Context context);
-    }
 
-    static class ConstructorInjectionProvider<T> implements ComponentProvider<T> {
-        private final Class<?> componentType;
-        private final Constructor<T> injectConstructor;
-
-        public ConstructorInjectionProvider(Class<?> componentType, Constructor<T> injectConstructor) {
-            this.componentType = componentType;
-            this.injectConstructor = injectConstructor;
-        }
-
-        @Override
-        public T get(Context context) {
-            try {
-                Object[] dependencies = stream(injectConstructor.getParameters())
-                        .map(p -> context.get(p.getType()).get())
-                        .toArray(Object[]::new);
-                return (T) injectConstructor.newInstance(dependencies);
-            } catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-    private static <Type> Constructor<Type> getInjectConstructor(Class<Type> implementation) {
-        List<Constructor<?>> injectConstructors = stream(implementation.getConstructors()).filter(c -> c.isAnnotationPresent(Inject.class)).collect(Collectors.toList());
-
-        if (injectConstructors.size() > 1) throw new IllegalComponentException();
-
-        return (Constructor<Type>) injectConstructors.stream().findFirst().orElseGet(() -> {
-            try {
-                return implementation.getConstructor();
-            } catch (NoSuchMethodException e) {
-                throw new IllegalComponentException();
-            }
-        });
+        List<Class<?>> getDependencies();
     }
 }
